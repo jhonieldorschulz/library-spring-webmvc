38a984b55d6c88547ac7915ff7711ff1a9977b63
Arthur Haupt Pacheco <arthurhauptpacheco@gmail.com>
2025-04-03 16:10:21 -0300

    Criado template autores

--- src/main/java/br/com/bpkedu/library_spring_webmvc/LibrarySpringWebmvcApplication.java
+++ src/main/java/br/com/bpkedu/library_spring_webmvc/LibrarySpringWebmvcApplication.java
@@ -3,6 +3,7 @@ package br.com.bpkedu.library_spring_webmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LibrarySpringWebmvcApplication {
 
--- src/main/java/br/com/bpkedu/library_spring_webmvc/domain/Livro.java
+++ src/main/java/br/com/bpkedu/library_spring_webmvc/domain/Livro.java
@@ -1,11 +1,9 @@
 package br.com.bpkedu.library_spring_webmvc.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "livros")
 public class Livro {
 
     @Id
